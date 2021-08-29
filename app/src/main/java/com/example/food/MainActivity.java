5b356dab5551424c849baecd266441f7bb416cd4
pema1000 <pemagtamang1000@gmail.com>
2021-08-29 17:18:35 -0500

    changed icon and removed title bar

--- app/src/main/java/com/example/food/MainActivity.java
+++ app/src/main/java/com/example/food/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
 
         GotoLoginPage=findViewById(R.id.homelogout);
